9e4acd5fc6648af1720f28be82c360857b460b1c
Alessandro Frisone <alefri2004@gmail.com>
2025-05-28 11:55:23 +0200

    feat: milestone 1/2

---
+++ attriciAttori/src/main.ts
@@ -0,0 +1,27 @@
+type Person = {
+  readonly id: number,
+  readonly name: string,
+  birth_year: number,
+  death_year?: number,
+  biography: string,
+  image: string
+}
+
+type ActressNationality = 
+  | "American"
+  | "British"
+  | "Australian"
+  | "Israeli-American"
+  | "South African"
+  | "French"
+  | "Indian"
+  | "Israelli"
+  | "Spanish"
+  | "South Korean"
+  | "Chinese"
+
+type Actress = Person & {
+  most_famous_movies: [string, string, string],
+  awards: string,
+  nationality: ActressNationality,
+}
